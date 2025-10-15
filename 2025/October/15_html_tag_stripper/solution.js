20932f4e7fc5dfb5eec0ce953e70dd1a1b221483
carljustineoyales <carljustineoyales@gmail.com>
2025-10-16 02:17:47 +0800

    Add 20251015 coding challenge

---
+++ 2025/October/15_html_tag_stripper/solution.js
@@ -0,0 +1,45 @@
+// Idea: Look for < and > then remove everything in between including the < and > 
+
+// loop into an array of characters
+// when < is found loop to find >
+// then when > is found delete everything in between including < and >
+
+// ['<','a',' ','h','r','e','f','=','"','#','"','>','C','l','i','c','k',' ','h','e','r','e','<','/','a','>']
+// look for the start tag symbol save the index  
+// [['<'],'a',...] index = 0
+// then search for the end tag
+// [...'"',['>'],'C'...] index = 11
+// then change all contents beween 0 to 11 to an empty string
+// repeat until it reaches the end
+// after loop is done join the remaining content
+
+function stripTags(html) {
+  let split_string = html.split('')
+  let startTag = '<'
+  let endTag = '>'
+  let startIndex, endIndex
+
+  // loop into split_string
+  for(let i = 0; i < split_string.length; i++){
+    // split_string[i] value = startTag set the startIndex value
+    if(split_string[i] === startTag) startIndex = i
+    // split_string[i] value = endTag set the endIndex value
+    if(split_string[i] === endTag) endIndex = i
+
+    // check if startIndex and endIndex exists
+    // this will not start if either values doesnt exists
+    if(startIndex !== undefined && endIndex !== undefined){
+      // loop into split_string again but start at startIndex and end with endIndex
+      for(let j = startIndex; j <= endIndex; j++){
+        // change all content to empty string
+        split_string[j] = ''
+      }
+
+      // when loop ends reset the index values
+      startIndex = undefined
+      endIndex = undefined
+    }
+  }
+  return split_string.join('')
+}
+
